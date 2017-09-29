462317e9dd5054566a4de8f21bc196130306d733
Ran Mizrahi <me@ranmizrahi.com>
2017-09-29 12:05:46 +0300

    added bit

--- src/components/item/item.tsx
+++ src/components/item/item.tsx
@@ -1,3 +1,6 @@
 import * as React from 'react';
 
+/**
+ * Awesome and stateless item component. 
+ */
 export const Item: React.SFC = () => <li>I am Item</li>;
--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import noop from '../../utils/noop';
 import {Item} from '../item';
 
+/**
+ * Awesome List React component.
+ */
 export class List extends React.Component {
     public render() {
         noop();
